5e375af7949654157ce8ec80cbe3ccbf20ccf7e1
Marzie Sangtarash <Marzie.Sangtarash@rigidrobotics.dev>
2023-06-18 10:53:52 +0330

    Add configuration

---
+++ Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Post;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configuration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<CategoryEntity>
+{
+    public void Configure(EntityTypeBuilder<CategoryEntity> builder)
+    {
+        builder.HasIndex(category => new { category.Id, category.Name }).IsUnique();
+    }
+}
